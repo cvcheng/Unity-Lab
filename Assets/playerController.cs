5b37f7ddc32599f9fbd7171e5ae4c2de9cda4581
Alan <cvcheng23@yahoo.com>
2016-11-02 05:01:46 -0400

    first commit

---
+++ Assets/playerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour
+{
+
+    public Rigidbody2D rigid;
+    public float speed = 7.0f;
+    public bool isFacingRight = false;
+    public float force = 5.0f;
+    public bool jump;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame, better for input
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    // FixedUpdate is better for physics and movement
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rigid.velocity.y);
+        rigid.velocity = new Vector2(rigid.velocity.x, Input.GetAxis("Vertical") * speed);
+
+        if ((Input.GetAxis("Horizontal") < 0 && !isFacingRight) || (Input.GetAxis("Horizontal") > 0 && isFacingRight))
+            Flip();
+
+        if (jump)
+        {
+            //rigid.velocity = new Vector2(rigid.velocity.x, 0);
+            rigid.transform.Translate(Vector3.up * 75 * Time.deltaTime, Space.World);
+
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 playerScale = transform.localScale;
+        playerScale.x = playerScale.x * -1;
+        transform.localScale = playerScale;
+        isFacingRight = !isFacingRight;
+    }
+
+}
